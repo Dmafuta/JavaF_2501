40d5aa5ec4f6c5c32bc07d33edfbe3ea42946651
Dmafuta <dmafuta07@gmail.com>
2025-10-29 09:43:56 +0300

    Create the repository for the Java Fundamentals Module

---
+++ src/sess01_intro2java/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sess01_intro2java;
+
+/**
+ *First Java program to display "Hello World" on the console/terminal
+ * @author Administrator
+ */
+public class HelloWorld {
+    public void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
