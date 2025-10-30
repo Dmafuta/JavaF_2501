9ff2ad197e05b766483f67043962f452b1c2be10
Dmafuta <dmafuta07@gmail.com>
2025-10-30 17:55:19 +0300

    some adjustments

--- src/sess01_intro2java/GreetUser.java
+++ src/sess01_intro2java/GreetUser.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-        
 package sess01_intro2java;
 
 // Bring in the package that contains a class to get input from the user
