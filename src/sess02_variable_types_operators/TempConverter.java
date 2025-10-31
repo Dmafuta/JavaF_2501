1e4d9b1587bb9a94a616864a375c5b92d3ba636a
Dmafuta <dmafuta07@gmail.com>
2025-10-31 11:08:30 +0300

    Initial commit

---
+++ src/sess02_variable_types_operators/TempConverter.java
@@ -0,0 +1,12 @@
+package sess02_variable_types_operators;
+
+/**
+ * Java program that accepts the temprature in degree Celsius and converts it to Degrees fahrenheit
+ * @author Administrator
+ */
+public class TempConverter {
+    // main method begins program execution
+    public static void main(String[] args) {
+        // TODO: complete the program
+    }
+}
